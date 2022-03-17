32cd5280b54e665d9614200670d1abdbc30dcb0d
Davy de Lima Carvalho <davy.carvalho@ambevtech.com.br>
2022-03-16 21:07:51 -0300

    creating todoRepository and configuring program

--- Todo/Todo.Domain.Api/Program.cs
+++ Todo/Todo.Domain.Api/Program.cs
@@ -1,6 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using Todo.Domain.Handlers;
+using Todo.Domain.Infra.Contexts;
+using Todo.Domain.Infra.Repositories;
+using Todo.Domain.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
+
+ConfigureServices(builder);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+ConfigureApps(app);
 
-app.Run();
+void ConfigureServices(WebApplicationBuilder builder)
+{
+    builder.Services.AddControllers();
+    builder.Services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
+    builder.Services.AddTransient<ITodoRepository, TodoRepository>();
+    builder.Services.AddTransient<TodoHandler, TodoHandler>();
+}
+
+void ConfigureApps(WebApplication app)
+{
+    app.MapControllers();
+    app.Run();
+}
